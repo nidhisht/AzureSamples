aad662aa97a9991274acc9b9909c19e009a5a4ba
Nidhish Th <nidhish.th@accenture.com>
2020-01-08 18:33:05 +0530

    web api integration with azure sql

---
+++ csharp_dotnetcore/01.webapi-azuresql/Controllers/CustomersController.cs
@@ -0,0 +1,28 @@
+﻿using _01.webapi_azuresql.Entities;
+using _01.webapi_azuresql.Helper;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace _01.webapi_azuresql.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        // GET api/Customers
+        [HttpGet]
+        public ActionResult<IEnumerable<Customer>> Get()
+        {
+            SqlHelper sqlHelper = new SqlHelper();
+            return sqlHelper.GetCustomers();
+        }
+
+        // GET api/Customers/5
+        [HttpGet("{id}")]
+        public ActionResult<Customer> Get(int id)
+        {
+            SqlHelper sqlHelper = new SqlHelper();
+            return sqlHelper.GetCustomer(id);
+        }
+    }
+}
---
+++ csharp_dotnetcore/01.webapi-azuresql/Helper/SqlHelper.cs
@@ -0,0 +1,78 @@
+﻿using _01.webapi_azuresql.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace _01.webapi_azuresql.Helper
+{
+    public class SqlHelper
+    {
+        string connectionString = "Server=tcp:sqldb162.database.windows.net,1433;Initial Catalog=sqldb-handson;Persist Security Info=False;User ID=nidhish;Password=azure-jan2020;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        public List<Customer> GetCustomers()
+        {
+            List<Customer> customers = new List<Customer>();
+            string query = "select * from [SalesLT].[Customer]";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        Customer customer;
+                        while (reader.Read())
+                        {
+                            customer = FillCustomer(reader);
+                            customers.Add(customer);
+                        }
+                    }
+                }
+            }
+            return customers;
+        }
+
+        public Customer GetCustomer(int id)
+        {
+            Customer customer = new Customer();
+            string query = "select * from [SalesLT].[Customer] where CustomerId=" + id;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            customer = FillCustomer(reader);
+                        }
+                    }
+                }
+            }
+            return customer;
+        }
+
+        private Customer FillCustomer(SqlDataReader reader)
+        {
+            Customer customer = new Customer
+            {
+                CustomerID = (int)reader["CustomerID"],
+                NameStyle = (reader["NameStyle"] ?? string.Empty).ToString(),
+                Title = (reader["Title"] ?? string.Empty).ToString(),
+                FirstName = (reader["FirstName"] ?? string.Empty).ToString(),
+                MiddleName = (reader["MiddleName"] ?? string.Empty).ToString(),
+                LastName = (reader["LastName"] ?? string.Empty).ToString(),
+                Suffix = (reader["Suffix"] ?? string.Empty).ToString(),
+                CompanyName = (reader["CompanyName"] ?? string.Empty).ToString(),
+                SalesPerson = (reader["SalesPerson"] ?? string.Empty).ToString(),
+                EmailAddress = (reader["EmailAddress"] ?? string.Empty).ToString(),
+                Phone = (reader["Phone"] ?? string.Empty).ToString(),
+                PasswordHash = (reader["PasswordHash"] ?? string.Empty).ToString(),
+                PasswordSalt = (reader["PasswordSalt"] ?? string.Empty).ToString(),
+                rowguid = (Guid)(reader["rowguid"] ?? Guid.Empty),
+                ModifiedDate = (DateTime)(reader["ModifiedDate"] ?? DateTime.MinValue),
+            };
+            return customer;
+        }
+    }
+}
