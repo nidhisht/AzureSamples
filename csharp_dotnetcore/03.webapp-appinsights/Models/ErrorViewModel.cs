f01be9e6cdc5483d517d0ee6e3d7bea1c9b604d1
Nidhish Th <nidhish.th@accenture.com>
2020-01-12 08:25:33 +0530

    added AppInsights project

---
+++ csharp_dotnetcore/03.webapp-appinsights/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _03.webapp_appinsights.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
