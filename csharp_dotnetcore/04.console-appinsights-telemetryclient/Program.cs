bbd4682bc99e7596dcd0c881c7c30c4c74e01ed2
Nidhish Th <nidhish.th@accenture.com>
2020-01-14 15:58:00 +0530

    added Azure App Insights in .net core console app

---
+++ csharp_dotnetcore/04.console-appinsights-telemetryclient/Program.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.DependencyCollector;
+using Microsoft.ApplicationInsights.Extensibility;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace _04.console_appinsights_telemetryclient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TelemetryConfiguration configuration = TelemetryConfiguration.CreateDefault();
+
+            configuration.InstrumentationKey = "ce80c749-3173-4c94-af3b-17f7bb734a19";
+            configuration.TelemetryInitializers.Add(new HttpDependenciesParsingTelemetryInitializer());
+
+            var telemetryClient = new TelemetryClient(configuration);
+            using (InitializeDependencyTracking(configuration))
+            {
+                // run app...
+
+                telemetryClient.TrackTrace("Hello World!");
+                telemetryClient.TrackEvent("MainMethodEvent");
+                telemetryClient.TrackAvailability("MainMethodAvailability", DateTimeOffset.Now, TimeSpan.MinValue, "Console", true);
+
+                using (var httpClient = new HttpClient())
+                {
+                    // Http dependency is automatically tracked!
+                    httpClient.GetAsync("https://microsoft.com").Wait();
+                }
+
+            }
+
+            // before exit, flush the remaining data
+            telemetryClient.Flush();
+
+            // flush is not blocking so wait a bit
+            Task.Delay(5000).Wait();
+            Console.WriteLine("Hello World!");
+            Console.Read();
+        }
+
+        static DependencyTrackingTelemetryModule InitializeDependencyTracking(TelemetryConfiguration configuration)
+        {
+            var module = new DependencyTrackingTelemetryModule();
+
+            // prevent Correlation Id to be sent to certain endpoints. You may add other domains as needed.
+            module.ExcludeComponentCorrelationHttpHeadersOnDomains.Add("core.windows.net");
+            module.ExcludeComponentCorrelationHttpHeadersOnDomains.Add("core.chinacloudapi.cn");
+            module.ExcludeComponentCorrelationHttpHeadersOnDomains.Add("core.cloudapi.de");
+            module.ExcludeComponentCorrelationHttpHeadersOnDomains.Add("core.usgovcloudapi.net");
+            module.ExcludeComponentCorrelationHttpHeadersOnDomains.Add("localhost");
+            module.ExcludeComponentCorrelationHttpHeadersOnDomains.Add("127.0.0.1");
+
+            // enable known dependency tracking, note that in future versions, we will extend this list. 
+            // please check default settings in https://github.com/microsoft/ApplicationInsights-dotnet-server/blob/develop/WEB/Src/DependencyCollector/DependencyCollector/ApplicationInsights.config.install.xdt
+
+            module.IncludeDiagnosticSourceActivities.Add("Microsoft.Azure.ServiceBus");
+            module.IncludeDiagnosticSourceActivities.Add("Microsoft.Azure.EventHubs");
+
+            // initialize the module
+            module.Initialize(configuration);
+
+            return module;
+        }
+    }
+}
