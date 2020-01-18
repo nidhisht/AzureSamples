2b36489c6f8fb625f80610f56e00265af6bc5118
Nidhish Th <nidhish.th@accenture.com>
2020-01-18 18:23:52 +0530

    Run a background task with Web Job

---
+++ csharp_dotnetcore/06.webjob-adventureworks/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+
+namespace _06.webjob_adventureworks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Background task started..");
+            for (int counter=0; counter<100; counter++)
+            {
+                Console.WriteLine("{0} - {1}", DateTime.Now.ToString(), counter.ToString());
+                Thread.Sleep(300); //delay
+            }
+            Console.WriteLine("Background task finished..");
+            Console.Read();
+        }
+    }
+}
