5e8700440d891d53a045e4af3d073e0965d59b1d
Nidhish Th <nidhish.th@accenture.com>
2020-02-03 22:27:23 +0530

    added swagger project

---
+++ csharp_dotnetcore/07.webapi-swagger/Controllers/ValuesController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _07.webapi_swagger.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        /// <summary>
+        /// This method lists all the values
+        /// </summary>
+        /// <returns></returns>
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        /// <summary>
+        /// This method returns single value against an id
+        /// </summary>
+        /// <param name="id">Mandatory</param>
+        /// <returns></returns>
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+
+        /// <summary>
+        /// This method add new value
+        /// </summary>
+        /// <param name="value">Mandatory</param>
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        /// <summary>
+        /// This method updates existing value
+        /// </summary>
+        /// <param name="id">Mandatory</param>
+        /// <param name="value"></param>
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+
+        /// <summary>
+        /// This method deletes a value
+        /// </summary>
+        /// <param name="id">Mandatory</param>
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ csharp_dotnetcore/07.webapi-swagger/Startup.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace _07.webapi_swagger
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddSwaggerGen(x => {
+                x.SwaggerDoc(
+                    "v1",
+                    new Microsoft.OpenApi.Models.OpenApiInfo
+                    {
+                        Title = "webapi swagger",
+                        Contact = new Microsoft.OpenApi.Models.OpenApiContact
+                        {
+                            Name = "Nidhish",
+                            Email = "nidhish@swagger.com"
+                        },
+                        Description = "This is web api swagger documentation"
+                    });
+
+                x.IncludeXmlComments(AppDomain.CurrentDomain.BaseDirectory + @"07.webapi-swagger.xml");
+                 
+            });
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+            app.UseSwagger();
+            app.UseSwaggerUI(
+                x => x.SwaggerEndpoint("/swagger/v1/swagger.json", "This swagger v1 description"));
+        }
+    }
+}
