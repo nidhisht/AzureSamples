2b7b791e95a7b123457aaa8f83558f29eaf08ba3
Nidhish T <nidhish.t@synapse.com>
2020-03-26 12:55:12 +0530

    Added Redis sample project

---
+++ csharp_dotnetcore/08.webapi-redis/Controllers/CacheController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis;
+
+namespace _08.webapi_redis.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CacheController : ControllerBase
+    {
+        private readonly IDatabase database;
+        private readonly string key="redisKey";
+        public CacheController(IDatabase db)
+        {
+            database = db;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            if (database.KeyExists(key))
+                return database.StringGet(key);
+            else
+                return "cache is empty";
+        }
+
+
+        [HttpGet("{value}")]
+        public string Set(string value)
+        {
+            database.StringSet(key, value);
+            return "added to cache";
+        }
+    }
+}
---
+++ csharp_dotnetcore/08.webapi-redis/Program.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace _08.webapi_redis
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
